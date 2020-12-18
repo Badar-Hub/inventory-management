98296183ec6249e63e84bdf5eb6bbe13cdeef231
Badar <badar@devs-MacBook-Pro.local>
2020-12-17 22:03:06 -0500

    added base classes

---
+++ src/components/base/Base.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import container from "@/IoC/config";
+import { Container } from "inversify";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class OnoBase extends Vue {
+  public container: Container = container;
+}
+</script>
---
+++ src/components/base/OnoMetadataBase.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { metadata } from "@/models";
+import { MetadataService } from "@/services/implementations";
+import OnoBase from "./Base.vue";
+
+export default class OnoMetadataBase extends OnoBase {
+  public isLoadingMetadata: boolean = true;
+
+  public metadataService!: MetadataService;
+
+  public formMetadata!: metadata;
+
+  public modelMetadataPath: string = "";
+
+  initMetadata() {
+    this.metadataService = this.container.get(MetadataService);
+    this.metadataService.getModelMetadata(this.modelMetadataPath).then(data => {
+      this.formMetadata = data;
+      this.isLoadingMetadata = false;
+    });
+  }
+}
+</script>
